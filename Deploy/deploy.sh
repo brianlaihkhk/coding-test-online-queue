b1f3d70b225af90774a175e4561271a923576513
Brian <brianlaihkhk@gmail.com>
2022-03-13 22:04:09 +0800

    Add AWS IAM policy and fix serverless deployment bug

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -8,33 +8,46 @@ cp -rf serverless/ build/
 mkdir build/Order/
 cp -rf ../Order/ build/Order/
 
-pip3 install -I -t build/ -r ../package.txt
+pip3 install -I -t build/ -r ../requirement.txt
 
 cd build/
 npm install serverless@1.73.1
 npm install serverless-python-requirements
 npm install serverless-prune-plugin
 npm install serverless-scriptable-plugin
-brew install mysql
-brew install openssl
-LDFLAGS=-L/usr/local/opt/openssl/lib pip install mysqlclient
+
+if [[ $(type -a mysql) =~ "not found" ]]; then
+    brew install mysql
+fi
+if [[ $(type -a openssl) =~ "not found" ]]; then
+    brew install openssl
+fi
+LDFLAGS=-L/usr/local/opt/openssl/lib pip3 install mysqlclient
 
 case "$2" in
     -g|--group)
         if [[ $3 -eq "all" ]]
         then
-            serverless deploy --function purchase
-            serverless deploy --function queue
-            serverless deploy --function session
-        elif [[ $3 -eq "purchase" ]]
+            serverless deploy function --function ping
+            serverless deploy function --function order
+            serverless deploy function --function status
+            serverless deploy function --function session
+            serverless deploy function --function item
+        elif [[ $3 -eq "item" ]]
         then
-            serverless deploy --function purchase
-        elif [[ $3 -eq "queue" ]]
+            serverless deploy function --function item
+        elif [[ $3 -eq "order" ]]
         then
-            serverless deploy --function queue
+            serverless deploy function --function order
+        elif [[ $3 -eq "status" ]]
+        then
+            serverless deploy function --function status
         elif [[ $3 -eq "session" ]]
         then
-            serverless deploy --function session
+            serverless deploy function --function session
+        elif [[ $3 -eq "ping" ]]
+        then
+            serverless deploy function --function ping
         else
             serverless $1 $2 $3 $4 $5 
         fi
