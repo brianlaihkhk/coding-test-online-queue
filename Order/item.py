17d31ffca1d4c65a838e01d478c02c32385dc3c4
Brian <brianlaihkhk@gmail.com>
2022-03-17 20:32:49 +0800

    Add additional logging in server side

--- Order/item.py
+++ Order/item.py
@@ -1,10 +1,13 @@
 from orm import Item
 from db import db
 import response
+import logging
 
 def list(event, context):
+    logging.info("Start getting items")
     items = Item.query.all()
     return_items = []
     for item in items:
         return_items.append({"item_uuid" : str(item.ITEM_UUID), "item_name" : str(item.ITEM_NAME), "item_description" : str(item.ITEM_DESCRIPTION) , "price" : float(item.PRICE)})
+    logging.info("Success in getting items")
     return response.success(return_items)
